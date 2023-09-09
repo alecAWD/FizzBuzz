80f153386eba29da44228ee0f30c56b182d7ce4f
Alec Dady <alec.dady9@gmail.com>
2023-09-09 15:55:43 -0400

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program {
+
+        public void FizzBuzz() {
+            for(int i = 1; i <= 100; i++) {
+                if(i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine(i + ": FizzBuzz");
+                }else if(i % 3 == 0) {
+                    Console.WriteLine(i + ": Fizz");
+                }else if(i % 5 == 0) {
+                    Console.WriteLine(i + ": Buzz");
+                }
+            }
+        }
+
+        public void OptimizedFizzBuzz() {
+            for(int i = 1; i <= 100; i++) {
+                string result = "";
+
+                if (i % 3 == 0) {
+                    result += "Fizz";
+                }
+                if(i % 5 == 0) {
+                    result += "Buzz";
+                }
+
+                if(result != "") {
+                    Console.WriteLine(i + ": " + result);
+                }
+            }
+        }
+        static void Main(string[] args) {
+            Program program = new Program();
+            program.OptimizedFizzBuzz();
+        }
+    }
+}
